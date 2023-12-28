e884a5c3c0ac4fbc7422f5d71f835e90d22b421d
Raj sharma <kevinrinta2018@gmail.com>
2023-12-28 12:20:45 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/welcome/index.js
+++ screens/welcome/index.js
@@ -1,50 +1,80 @@
-import React, { useEffect, useState } from "react";
-import { SafeAreaView, View, Text, Image, FlatList, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { SafeAreaView, View, Text, FlatList, Image, StyleSheet } from "react-native";
 
-const ProductListingScreen = () => {
-  const [products, setProducts] = useState([]);
-  useEffect(() => {
-    fetch("https://dummyjson.com/products/1").then(response => response.json()).then(data => setProducts(data)).catch(error => console.error(error));
-  }, []);
+const ListScreen = () => {
+  const [data, setData] = useState([{
+    id: 1,
+    title: "iPhone 9",
+    description: "An apple mobile which is nothing like apple",
+    price: 549,
+    discountPercentage: 12.96,
+    rating: 4.69,
+    stock: 94,
+    brand: "Apple",
+    category: "smartphones",
+    thumbnail: "https://i.dummyjson.com/data/products/1/thumbnail.jpg",
+    images: ["https://i.dummyjson.com/data/products/1/1.jpg", "https://i.dummyjson.com/data/products/1/2.jpg", "https://i.dummyjson.com/data/products/1/3.jpg", "https://i.dummyjson.com/data/products/1/4.jpg", "https://i.dummyjson.com/data/products/1/thumbnail.jpg"]
+  }]);
 
   const renderItem = ({
     item
   }) => <View style={styles.itemContainer}>
-      <Image style={styles.image} source={{
-      uri: item.image
-    }} />
-      <Text style={styles.title}>{item.title}</Text>
-      <Text style={styles.price}>{item.price}</Text>
+      <Image source={{
+      uri: item.thumbnail
+    }} style={styles.imageStyle} />
+      <View style={styles.textContainer}>
+        <Text style={styles.titleStyle}>{item.title}</Text>
+        <Text style={styles.descriptionStyle}>{item.description}</Text>
+        <Text style={styles.priceStyle}>${item.price}</Text>
+      </View>
     </View>;
 
   return <SafeAreaView style={styles.container}>
-      <FlatList data={products} renderItem={renderItem} keyExtractor={item => item.id.toString()} />
+      <FlatList data={data} renderItem={renderItem} keyExtractor={item => item.id.toString()} />
     </SafeAreaView>;
 };
 
+export default ListScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff"
+    backgroundColor: "#F5F5F5"
   },
   itemContainer: {
     flexDirection: "row",
     padding: 10,
-    borderBottomWidth: 1,
-    borderBottomColor: "#ccc"
+    margin: 10,
+    backgroundColor: "#FFFFFF",
+    borderRadius: 13,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5
   },
-  image: {
-    width: 50,
-    height: 50,
-    marginRight: 10
+  imageStyle: {
+    width: 70,
+    height: 70,
+    borderRadius: 10
   },
-  title: {
+  textContainer: {
+    marginLeft: 10,
+    justifyContent: "space-between"
+  },
+  titleStyle: {
     fontSize: 16,
     fontWeight: "bold"
   },
-  price: {
-    fontSize: 14,
-    color: "#888"
+  descriptionStyle: {
+    fontSize: 12,
+    color: "#888888"
+  },
+  priceStyle: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "#000000"
   }
 });
-export default ProductListingScreen;
