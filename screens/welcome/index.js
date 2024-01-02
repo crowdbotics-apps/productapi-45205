1c272ed964b57903f5b4bb1a9cd54942a1636ff2
Raj sharma <kevinrinta2018@gmail.com>
2024-01-02 14:59:50 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/welcome/index.js
+++ screens/welcome/index.js
@@ -1,99 +1,17 @@
-import React, { useState } from "react";
-import { SafeAreaView, View, Text, FlatList, Image, StyleSheet, TouchableOpacity, TextInput, Button } from "react-native";
+import React, { useState, useEffect } from "react";
+import { SafeAreaView, View, Text, FlatList, Image, StyleSheet, TouchableOpacity } from "react-native";
 
 const ListScreen = ({
   navigation
 }) => {
-  const [data, setData] = useState([{
-    id: 1,
-    title: "iPhone 6",
-    description: "An apple mobile which is nothing like apple",
-    price: 400,
-    discountPercentage: 12.96,
-    rating: 4.69,
-    stock: 94,
-    brand: "Apple",
-    category: "smartphones",
-    thumbnail: "https://i.dummyjson.com/data/products/1/thumbnail.jpg",
-    images: ["https://i.dummyjson.com/data/products/1/1.jpg", "https://i.dummyjson.com/data/products/1/2.jpg", "https://i.dummyjson.com/data/products/1/3.jpg", "https://i.dummyjson.com/data/products/1/4.jpg", "https://i.dummyjson.com/data/products/1/thumbnail.jpg"]
-  }, {
-    id: 2,
-    title: "iPhone 9",
-    description: "An apple mobile which is nothing like apple",
-    price: 549,
-    discountPercentage: 12.96,
-    rating: 4.69,
-    stock: 94,
-    brand: "Apple",
-    category: "smartphones",
-    thumbnail: "https://i.dummyjson.com/data/products/1/thumbnail.jpg",
-    images: ["https://i.dummyjson.com/data/products/1/1.jpg", "https://i.dummyjson.com/data/products/1/2.jpg", "https://i.dummyjson.com/data/products/1/3.jpg", "https://i.dummyjson.com/data/products/1/4.jpg", "https://i.dummyjson.com/data/products/1/thumbnail.jpg"]
-  }, {
-    id: 3,
-    title: "iPhone 10",
-    description: "An apple mobile which is nothing like apple",
-    price: 600,
-    discountPercentage: 12.96,
-    rating: 4.69,
-    stock: 94,
-    brand: "Apple",
-    category: "smartphones",
-    thumbnail: "https://i.dummyjson.com/data/products/1/thumbnail.jpg",
-    images: ["https://i.dummyjson.com/data/products/1/1.jpg", "https://i.dummyjson.com/data/products/1/2.jpg", "https://i.dummyjson.com/data/products/1/3.jpg", "https://i.dummyjson.com/data/products/1/4.jpg", "https://i.dummyjson.com/data/products/1/thumbnail.jpg"]
-  }, {
-    "id": 5,
-    "title": "Huawei P30",
-    "description": "Huawei’s re-badged P30 Pro New Edition was officially unveiled yesterday in Germany and now the device has made its way to the UK.",
-    "price": 499,
-    "discountPercentage": 10.58,
-    "rating": 4.09,
-    "stock": 32,
-    "brand": "Huawei",
-    "category": "smartphones",
-    "thumbnail": "https://i.dummyjson.com/data/products/5/thumbnail.jpg",
-    "images": ["https://i.dummyjson.com/data/products/5/1.jpg", "https://i.dummyjson.com/data/products/5/2.jpg", "https://i.dummyjson.com/data/products/5/3.jpg"]
-  }, {
-    "id": 6,
-    "title": "MacBook Pro",
-    "description": "MacBook Pro 2021 with mini-LED display may launch between September, November",
-    "price": 1749,
-    "discountPercentage": 11.02,
-    "rating": 4.57,
-    "stock": 83,
-    "brand": "Apple",
-    "category": "laptops",
-    "thumbnail": "https://i.dummyjson.com/data/products/6/thumbnail.png",
-    "images": ["https://i.dummyjson.com/data/products/6/1.png", "https://i.dummyjson.com/data/products/6/2.jpg", "https://i.dummyjson.com/data/products/6/3.png", "https://i.dummyjson.com/data/products/6/4.jpg"]
-  }, {
-    "id": 7,
-    "title": "Samsung Galaxy Book",
-    "description": "Samsung Galaxy Book S (2020) Laptop With Intel Lakefield Chip, 8GB of RAM Launched",
-    "price": 1499,
-    "discountPercentage": 4.15,
-    "rating": 4.25,
-    "stock": 50,
-    "brand": "Samsung",
-    "category": "laptops",
-    "thumbnail": "https://i.dummyjson.com/data/products/7/thumbnail.jpg",
-    "images": ["https://i.dummyjson.com/data/products/7/1.jpg", "https://i.dummyjson.com/data/products/7/2.jpg", "https://i.dummyjson.com/data/products/7/3.jpg", "https://i.dummyjson.com/data/products/7/thumbnail.jpg"]
-  }, {
-    "id": 8,
-    "title": "Microsoft Surface Laptop 4",
-    "description": "Style and speed. Stand out on HD video calls backed by Studio Mics. Capture ideas on the vibrant touchscreen.",
-    "price": 1499,
-    "discountPercentage": 10.23,
-    "rating": 4.43,
-    "stock": 68,
-    "brand": "Microsoft Surface",
-    "category": "laptops",
-    "thumbnail": "https://i.dummyjson.com/data/products/8/thumbnail.jpg",
-    "images": ["https://i.dummyjson.com/data/products/8/1.jpg", "https://i.dummyjson.com/data/products/8/2.jpg", "https://i.dummyjson.com/data/products/8/3.jpg", "https://i.dummyjson.com/data/products/8/4.jpg", "https://i.dummyjson.com/data/products/8/thumbnail.jpg"]
-  }]);
-  const [search, setSearch] = useState("");
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch("https://dummyjson.com/products").then(response => response.json()).then(json => setData(json)).catch(error => console.error(error));
+  }, []);
 
   const renderItem = ({
     item
-  }) => <TouchableOpacity style={styles.itemContainer} onPress={() => navigation.navigate("Details", {
+  }) => <TouchableOpacity style={styles.itemContainer} onPress={() => navigation.navigate("ProductDetails", {
     item
   })}>
       <Image source={{
@@ -106,14 +24,8 @@ const ListScreen = ({
       </View>
     </TouchableOpacity>;
 
-  const filterData = data.filter(item => item.title.toLowerCase().includes(search.toLowerCase()));
-  const sortedData = filterData.sort((a, b) => b.price - a.price);
   return <SafeAreaView style={styles.container}>
-      <View style={styles.searchContainer}>
-        <TextInput style={styles.searchInput} value={search} placeholder="Search by title..." onChangeText={setSearch} />
-        <Button title="Sort by price" onPress={() => setData(sortedData)} />
-      </View>
-      <FlatList data={filterData} renderItem={renderItem} keyExtractor={item => item.id.toString()} />
+      <FlatList data={data} renderItem={renderItem} keyExtractor={item => item.id.toString()} />
     </SafeAreaView>;
 };
 
@@ -159,18 +71,5 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontWeight: "bold",
     color: "#000000"
-  },
-  searchContainer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    padding: 10
-  },
-  searchInput: {
-    flex: 1,
-    borderColor: "#000",
-    borderWidth: 1,
-    marginRight: 10,
-    borderRadius: 5,
-    padding: 5
   }
 });
